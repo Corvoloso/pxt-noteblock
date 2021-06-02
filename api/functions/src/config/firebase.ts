9ede4050027c155e0e1c7f2f874c54c77f4420e9
Corvoloso <igoralves.b18@gmail.com>
2021-06-01 22:20:59 -0300

    Firebase Function Application Concluded

---
+++ api/functions/src/config/firebase.ts
@@ -0,0 +1,15 @@
+import firebase from "firebase/app"
+
+import "firebase/auth"
+import "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBBhxtQeWsc84ysfAmmzLuttKjdhHMPNQ0",
+  authDomain: "pxt-noteblock.firebaseapp.com",
+  projectId: "pxt-noteblock",
+  storageBucket: "pxt-noteblock.appspot.com",
+  messagingSenderId: "661902995460",
+  appId: "1:661902995460:web:a9ebca1870722245b50551",
+}
+
+export default firebase.initializeApp(firebaseConfig)
---
+++ api/functions/src/index.ts
@@ -0,0 +1,107 @@
+import * as functions from "firebase-functions"
+import firebase from "./config/firebase"
+
+const db = firebase.firestore()
+
+interface messageProps {
+  id: string;
+  message: string;
+}
+
+export const authenticate = functions.https.onRequest(async (request, response) => {
+  const { email, password } = request.body
+
+  try {
+    const userAuthenticated = await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+
+    response.status(200).json(userAuthenticated)
+  } catch (err) {
+    response.status(400).json({
+      err,
+      message: "Erro ao tentar se autenticar na rede"
+    })
+  }
+})
+
+export const getMessages = functions.https.onRequest(async (_, response) => {
+  const messagesFound = await db.collection("messages").get()
+
+  const formattedMessagesFound: messageProps[] = []
+
+  messagesFound.docs.map((message: any) => {
+    const data: any = {
+      id: message.id,
+      ...message.data()
+    }
+
+    formattedMessagesFound.push(data)
+  })
+
+  response.status(200).json({
+    messages: formattedMessagesFound,
+  })
+})
+
+export const postMessage = functions.https.onRequest(async (request, response) => {
+  const { message } = request.body
+
+  try {
+    const newMessageRef = await db.collection("messages").add({
+      message,
+    })
+
+    response.status(200).json({
+      success: "Mensagem salva com sucesso!",
+      message: {
+        id: newMessageRef.id,
+        message: message
+      }
+    })
+  } catch (err) {
+    response.status(400).json({
+      err
+    })
+  }
+})
+
+export const putMesssage = functions.https.onRequest(async (request, response) => {
+  const { id } = request.params
+  const { message } = request.body
+
+  try {
+    const messageRef = db.collection("messages").doc(id)
+
+    await messageRef.update({
+      message
+    })
+
+    response.status(200).json({
+      success: "Mensagem atualizada com sucesso"
+    })
+  } catch (err) {
+    response.status(400).json({
+      err
+    })
+  }
+})
+
+export const deleteMessage = functions.https.onRequest(async (request, response) => {
+  const { id } = request.params
+
+
+  try {
+    const messageRef = db.collection("messages").doc(id)
+
+    await messageRef.delete()
+
+    response.status(200).json({
+      success: "Mensagem deletada com sucesso"
+    })
+  } catch (err) {
+    response.status(400).json({
+      err
+    })
+  }
+})
